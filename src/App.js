7f970e896b160a3932ec905046192c6d1bcea0db
azhan3 <azhan3@ocdsb.ca>
2022-11-23 00:50:50 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Harry Wang AI
+          <code>Harry Wang
             - In development
           </code>
         </p>
